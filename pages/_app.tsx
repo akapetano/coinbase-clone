40be1dbba92db211298bc01111bdfd0a576c6ca3
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-04-13 11:45:53 +0200

    add ThirdwebProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+const supportedChainIds = 4;
+// const connectors = {
+//   injected: {},
+// };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId={supportedChainIds}
+      // connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
